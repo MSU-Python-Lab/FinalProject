81ae104bb703d373cc37764e6cb723af5ca2308a
illus <illusory.racer01@mail.ru>
2022-06-15 08:27:39 +1000

    Changed the project framework to Next js React Added working logic for Registration and Authentication

---
+++ IconicClientReact/pages/_app.js
@@ -0,0 +1,33 @@
+// import Index from 'next/app'
+import '../styles/globals.css'
+//import '../styles/stylesMain.css'
+import '../styles/regpage.css'
+import '../components/ModalWindow/modal.css'
+import { Provider } from 'react-redux'
+import { useStore } from '../core/store'
+import { ChakraProvider } from '@chakra-ui/react'
+
+
+export default function MyApp({ Component, pageProps }) {
+    const store = useStore(pageProps.initialReduxState)
+
+    return (
+        <Provider store={store}>
+            <ChakraProvider>
+                <Component {...pageProps} />
+            </ChakraProvider>
+        </Provider>
+    )
+}
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await Index.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
