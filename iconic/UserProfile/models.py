5a2034f96ff38c385589cb211181194953475c53
Hrustyaschiy <lyshenko2001@mail.ru>
2022-06-14 15:40:13 +1000

    /accounts/users/ GET and /accounts/data/ GET now will return id of user changed ManyToManyFields in serializers.py to ForeignKey field because ManyToManyFields cause a lot of errors and we do not know how to work with it

--- iconic/UserProfile/models.py
+++ iconic/UserProfile/models.py
@@ -18,8 +18,8 @@ class Cities(models.Model):
 
 
 class Followers(models.Model):
-    user_id = models.ManyToManyField(accounts_user.CustomUser, related_name='followers_user_id')
-    follower = models.ManyToManyField(accounts_user.CustomUser, related_name='followers_follower')
+    user_id = models.ForeignKey(accounts_user.CustomUser, on_delete=models.CASCADE, related_name='followers_user_id')
+    follower = models.ForeignKey(accounts_user.CustomUser, on_delete=models.CASCADE, related_name='followers_follower')
 
 
 class Post(models.Model):
@@ -34,8 +34,8 @@ class Post(models.Model):
 
 
 class Likes(models.Model):
-    user_id = models.ManyToManyField(accounts_user.CustomUser, related_name='likes_user_id')
-    post_id = models.ManyToManyField(Post, related_name='likes_post_id')
+    user_id = models.ForeignKey(accounts_user.CustomUser, on_delete=models.CASCADE, related_name='likes_user_id')
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes_post_id')
 
 
 class Wallet(models.Model):
