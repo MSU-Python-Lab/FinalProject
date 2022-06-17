8178f20ed442917c3fa96a6584d3ee3aecfb23f2
Hrustyaschiy <lyshenko2001@mail.ru>
2022-06-17 11:22:20 +1000

    Added email validation on put request on /accounts/data/

--- iconic/accounts/views.py
+++ iconic/accounts/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 from django.contrib.auth import get_user_model
 from rest_framework import generics, permissions
+from validate_email import validate_email
+from rest_framework.response import Response
+from rest_framework import status
 
 from . import serializers
 
@@ -14,10 +17,21 @@ class UserRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = serializers.CustomUserSerializer
     permission_classes = (permissions.IsAuthenticated,)
 
-    def perform_update(self, serializer):
-        instance = serializer.save()
-        instance.set_password(instance.password)
-        instance.save()
-
     def get_object(self):
         return self.request.user
+
+    def update(self, request, *args, **kwargs):
+        if not validate_email(request.data["email"]):
+            return Response({"email": "Данные не прошли проверку"}, status=status.HTTP_400_BAD_REQUEST)
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
